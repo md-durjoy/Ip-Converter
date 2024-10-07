03bf56da752654b3f2c1ea2d1546c87c48535518
MD Durjoy <heymrnoor@outlook.com>
2024-10-07 20:57:36 +0600

    Update ip-converter.py

--- ip-converter.py
+++ ip-converter.py
@@ -68,7 +68,7 @@ with open(domain_file, 'r') as file:
                         # Display loading animation with skull
                         for i in range(5):  # Adjust the range for more loading steps
                             print("-" * (i + 1) + "💀", end='\r')
-                            time.sleep(0.8)  
+                            time.sleep(0.3)  
                         
                         # Resolve domain name to IP address
                         ip = socket.gethostbyname(domain)
